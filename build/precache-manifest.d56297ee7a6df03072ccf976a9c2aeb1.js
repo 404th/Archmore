a960b56991affe1eb27381e1725536a536ffa685
Myevenway <umarov.doniyor.2002@gmail.com>
2020-08-16 14:20:21 +0500

    Web site has been added to firebase hosting

---
+++ build/precache-manifest.d56297ee7a6df03072ccf976a9c2aeb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc80a5b0b395412db197b07f7ad29ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2314435ae9fa4ac77275",
+    "url": "/static/css/main.e980a534.chunk.css"
+  },
+  {
+    "revision": "37a2c12259607ce104f8",
+    "url": "/static/js/2.93da3c43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93da3c43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2314435ae9fa4ac77275",
+    "url": "/static/js/main.dd06cbb6.chunk.js"
+  },
+  {
+    "revision": "3898008604463e448d8e",
+    "url": "/static/js/runtime-main.34c3041e.js"
+  }
+]);
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBprb_7lYSmrrDnmpm0dQvD7kn10llCHww",
+  authDomain: "archmore-e91dc.firebaseapp.com",
+  databaseURL: "https://archmore-e91dc.firebaseio.com",
+  projectId: "archmore-e91dc",
+  storageBucket: "archmore-e91dc.appspot.com",
+  messagingSenderId: "485787151118",
+  appId: "1:485787151118:web:e45cf81092cb3d7ddc855f",
+  measurementId: "G-RN7KHTSWM7"
+};
+firebase.initializeApp(firebaseConfig);
