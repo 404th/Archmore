22d91bc8b3ec82aedf48219ea979b7e08273f0d0
Myevenway <umarov.doniyor.2002@gmail.com>
2020-08-16 11:56:41 +0500

    Committed

--- src/components/signIn/signInPage.jsx
+++ src/components/signIn/signInPage.jsx
@@ -4,33 +4,7 @@ import './signIn.css'
 const SignIn = () => {
   return (
     <div className='signIn_container' >
-      <div className="signIn_container_cover">
-        <div className="signIn_container_cover_first">
-          <img src="./images/img/sign/Login_1.svg" width='100%' height='100%' alt="login"/>
-        </div>
-        <div className="signIn_container_cover_second">
-          <div className="signIn_container_cover_second_form">
-            <form action="post">
-              <div>
-                <div>
-                  <label>Username:</label>
-                </div>
-                <div>
-                  <input type="text" />
-                </div>
-              </div>
-              <div>
-                <div>
-                  <label>Password:</label>
-                </div>
-                <div>
-                  <input type="password" />
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+      Sign in
     </div>
   )
 }
